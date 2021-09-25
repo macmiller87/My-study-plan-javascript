24713433825e8f04727bfc0785d5a50a05b42f65
Macmiller <macamagolf@gmail.com>
2021-09-25 20:49:04 -0300

    Update file

--- javascript.js
+++ javascript.js
@@ -4,11 +4,12 @@ function CriarParagrafo() {
     paragrafo.textContent = 'Você clicou no botão !';
     document.body.appendChild(paragrafo);
     
-    const buttons = document.querySelectorAll('button');
+}
 
-    for(let looping = 0; looping < buttons.length; looping ++) {
 
-        buttons[looping].addEventListener('click', CriarParagrafo);
-    }
-    
+const buttons = document.querySelectorAll('button');
+
+for(let looping = 0; looping < buttons.length; looping ++) {
+
+    buttons[looping].addEventListener('click', CriarParagrafo);
 }
