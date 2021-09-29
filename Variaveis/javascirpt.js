9afe19dbe883320f8688c6c9de616d1b118bd742
Macmiller <macamagolf@gmail.com>
2021-09-29 02:22:02 -0300

    update file

---
+++ Variaveis/javascirpt.js
@@ -0,0 +1,8 @@
+var button = document.querySelector('button');
+
+button.onclick = function() {
+
+    var nome = prompt('Qual seu nome ?');
+    alert('Olá ' + nome + ', é um prazer te ver !');
+    
+}
