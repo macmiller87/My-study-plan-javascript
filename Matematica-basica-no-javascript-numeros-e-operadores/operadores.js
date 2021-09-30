34e145c45f27efbcf0db804a69b35688e96f86e1
Macmiller <macamagolf@gmail.com>
2021-09-30 09:40:13 -0300

    creating folder

---
+++ Matematica-basica-no-javascript-numeros-e-operadores/operadores.js
@@ -0,0 +1,69 @@
+/* Operadores basicos nas linguagens */
+
+/* Operadores Aritméticos */
+
+/* 
+
+OPERADOR	NOME	            PROPÓSITO	                                        EXEMPLO
+
+  +	        Adição	        Adiciona um número a outro.	                            6 + 9
+  -	        Subtração	    Subtrai o número da direita do número da esquerda.	    20 - 15
+  *     	Multiplicação	Multiplica um número pelo outro.	                    3 * 7
+  /	        Divisão     	Divide o número da esquerda pelo número da direita.	    10 / 5
+  %	        Restante        (Remainder - as vezes chamado de modulo)	            8 % 3 (retorna 2; como três cabe duas vezes em 8, deixando 2 como resto.)
+                            Retorna o resto da divisão em números inteiros 
+                            do número da esquerda pelo número da direita.
+*/
+
+
+
+/* Operadores de incremento e decremento */
+
+/*
+
+OPERADOR    NOME        
+
+ ++       Incremento
+ --       Decremento
+
+*/
+
+
+
+/* Operadores de atribuição */
+
+
+/*
+
+OPERADOR	    NOME	                    PROPÓSITO	                                                                 EXEMPLO
+
+  +=	        Atribuição de adição	    Adiciona o valor à direita ao valor da variável à esquerda e,       x = 3;   x = 3;
+                                            em seguida, retorna o novo valor da variável	                    x += 4;	 x = x + 4;
+
+  -=	        Atribuição de subtração	    Subtrai o valor à direita do valor da variável à esquerda e         x = 6;	 x = 6; 
+                                            retorna o novo valor da variável	                                x -= 3;  x = x - 3;
+
+  *=	        Atribuição de multiplicação	Multiplica o valor da variável à esquerda pelo valor à direita e    x = 2;   x = 2; 
+                                            retorna o novo valor da variável                                    x *= 3   x = x * 3;
+
+  /=	        Atribuição de divisão	    Divide o valor da variável à esquerda pelo valor à direita e        x = 10;  x = 10;
+                                            retorna o novo valor da variável                                    x /= 5;  x = x / 5;                                      
+
+*/
+
+
+/* Operadores de comparação */
+
+
+/* 
+
+OPERADOR	    NOME	                    PROPÓSITO	                                                                 EXEMPLO
+
+ ===	        Igualdade estrita	        Verifica se o valor da esquerda e o da direita são idênticos entre si.	     5 === 2 + 4
+ !==	        Não-igualdade-estrita	    Verifica se o valor da esquerda e o da direita não são idênticos entre si.	 5 !== 2 + 3
+ <	            Menor que	                Verifica se o valor da esquerda é menor que o valor da direita.	             10 < 6
+ >	            Maior que	                Verifica se o valor da esquerda é maior que o valor da direita.	             10 > 20
+ <=	            Menor ou igual que	        Verifica se o valor da esquerda é menor que ou igual ao valor da direita.	 3 <= 2
+ >=	            Maior ou igual que	        Verifica se o valor da esquerda é maior que ou igual ao valor da direita.	 5 >= 4
+ 
+*/
