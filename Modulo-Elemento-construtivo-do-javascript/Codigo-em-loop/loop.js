c1016f4deeb035cbf13c14d947541ed614a949bf
Macmiller Duarte de Andrade <macamagolf@gmail.com>
2021-10-13 15:11:41 -0300

    update file

--- Modulo-Elemento-construtivo-do-javascript/Codigo-em-loop/loop.js
+++ Modulo-Elemento-construtivo-do-javascript/Codigo-em-loop/loop.js
@@ -25,3 +25,42 @@ for (var i = 0; i < cats.length; i++) {
     }
 
 }
+
+/* Mesmo exemplo de uso do algortimo acima, mas usando o laço While */
+
+
+var i = 0;
+
+while (i < cats.length) {
+  
+  if (i === cats.length - 1) {
+    info += 'and ' + cats[i] + '.';
+    
+  } else {
+    
+    info += cats[i] + ', ';
+    
+  }
+  i++;
+}
+
+
+/* Mesmo exemplo de uso dos algortimos acima, mas usando o laço do While */
+
+
+var i = 0;
+
+do {
+  
+  if (i === cats.length - 1) {
+    
+    info += 'and ' + cats[i] + '.';
+    
+  } else {
+    
+    info += cats[i] + ', ';
+    
+  }
+  i++;
+  
+} while (i < cats.length);
