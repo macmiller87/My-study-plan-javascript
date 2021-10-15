e9591c1722ccc3b841968c2af9e95c99c2ee5254
Macmiller <macamagolf@gmail.com>
2021-10-15 09:24:55 -0300

    update file

---
+++ Modulo-Elemento-construtivo-do-javascript/Funcoes-blocos-reutilizaveis-de-codigo/Aprendizado-ativo/main.js
@@ -0,0 +1,21 @@
+let x = 1;
+
+function a() {
+
+  let y = 2;
+  
+}
+
+function b() {
+
+  let z = 3;
+
+}
+
+function output(value) {
+
+  const para = document.createElement('p');
+  document.body.appendChild(para);
+  para.textContent = 'Value: ' + value;
+
+}
