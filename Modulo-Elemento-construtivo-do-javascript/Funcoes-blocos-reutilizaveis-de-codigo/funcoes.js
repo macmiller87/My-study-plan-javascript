f5d2e5bf3ebbc1df38352d617dbe649f6abf5bab
Macmiller <macamagolf@gmail.com>
2021-10-16 02:48:15 -0300

    update file

--- Modulo-Elemento-construtivo-do-javascript/Funcoes-blocos-reutilizaveis-de-codigo/funcoes.js
+++ Modulo-Elemento-construtivo-do-javascript/Funcoes-blocos-reutilizaveis-de-codigo/funcoes.js
@@ -43,3 +43,35 @@ var myGreeting = function() {
 }
 
 myGreeting();
+
+
+/* Exemplo de funções dentro de funções */
+
+
+function myBigFunction() {
+
+    var myValue = 1;
+  
+    subFunction1(myValue);
+    subFunction2(myValue);
+    subFunction3(myValue);
+
+}
+  
+function subFunction1(value) {
+
+    console.log(value);
+
+}
+  
+function subFunction2(value) {
+
+    console.log(value);
+
+}
+  
+function subFunction3(value) {
+
+    console.log(value);
+
+}
