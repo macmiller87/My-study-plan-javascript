61ec457b65be0878824413aaf2b70eb87fb33642
Macmiller Duarte de Andrade <macamagolf@gmail.com>
2021-10-27 17:33:16 -0300

    update file

--- Modulo-introducao-a-objetos-em-javascript/Javascript-orientado-a-objetos-para-iniciantes/main.js
+++ Modulo-introducao-a-objetos-em-javascript/Javascript-orientado-a-objetos-para-iniciantes/main.js
@@ -52,3 +52,47 @@ person2.greeting()
 /* Método usado para chamar os objetos  greeting() */
 
 greeting()
+
+
+
+/* Também é possível declarar o objeto desta forma,  Exemplo de como pode ser de clarado um bojeto */
+
+
+var person1 = new Object(); // Declaração da variavel person1 Com o método construtor Object(), que já torna a variavel person1 um objeto, sendo possível atribuir os seus atributos.
+
+
+/* Neste caso estamos/atribuindo setando os atributos da variavel person1 acima */
+
+person1.name = 'Chris';
+person1['age'] = 38;
+
+person1.greeting = function() {
+  
+alert('Hi! I\'m ' + this.name + '.');
+  
+};
+
+
+/* Também podemos fazer deta forma e já definir o Object() na própia variavel, e já inserir seus atributos  */ 
+
+
+var person1 = new Object({
+  name: 'Chris',
+  age: 38,
+  
+  greeting: function() {
+    
+    alert('Hi! I\'m ' + this.name + '.');
+  }
+});
+
+
+/* Existe também o método create(), com esse método é possível criar um novo objeto apartir de um objeto já criado, no exemplo abaixo vamos criar um novo objeto apartir do objeto person1, e podemos utilizar os seus atributos */
+
+var person2 = Object.create(person1); 
+
+
+/* Aqui estamos fazendo a chamada do objeto person2, e utilizando o atributo que foi setado no person1 .... name */ 
+
+person2.name
+person2.greeting()
