aa1ecc1e27479d41a0af4b4b8395bac0118c73f0
Macmiller <macamagolf@gmail.com>
2021-10-27 09:48:58 -0300

    update file

--- Modulo-introducao-a-objetos-em-javascript/Javascript-orientado-a-objetos-para-iniciantes/main2.js
+++ Modulo-introducao-a-objetos-em-javascript/Javascript-orientado-a-objetos-para-iniciantes/main2.js
@@ -12,11 +12,25 @@ function Person(first, last, age, gender, interests) {
     this.age = age;
     this.gender = gender;
     this.interests = interests;
-    this.bio = function() {
 
-      alert(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+    if(this.gender == 'man' || this.gender == 'm' || this.gender == 'M')  {
 
-    };
+      this.bio = function() {
+
+        alert('I am a ' + this.gender + ' ' + this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+  
+      };
+
+    }else {
+
+      this.bio = function() {
+
+        alert('I am a ' + this.gender + ' ' + this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. She likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+  
+      };
+
+    }
+
 
     this.greeting = function() {
       alert('Hi! I\'m ' + this.name.first + '.');
@@ -27,12 +41,13 @@ function Person(first, last, age, gender, interests) {
 
 /* Aqui está sendo setado o objeto person1, e sendo passado seus atributos */
 
-var person1 = new Person('Bob', 'Smith', 32, 'male', ['music', 'skiing']);
+var person1 = new Person('Bob', 'Smith', 32, 'M', ['music', 'skiing', 'computer', 'chocolate']);
 
 
 /* Aqui está sendo chamado a funçaõ bio, e os parametros da função Person, e sendo atribuido ao objeto person1 */
 
 person1['age']
+person1.gender
 person1.interests[1]
 person1.bio()
 // etc.
